a2c4342c63fd36164b840965a16b13704d3e1eba
virtual-frame-7789 <mttwright@gmail.com>
2019-08-29 22:20:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,6 @@
 def square_array(array)
   
-  count = 0
-  new_array = []
-  
-  
-  while array[count] do
-    new_array[count] = array[count] * array[count]
-    count +=
-  end
+
     
   
 end
